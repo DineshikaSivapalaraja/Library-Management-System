94a4fbec1a2cb0dec623cf8cafad9e7a4e1a580a
Dineshika <99dinuraja@gmail.com>
2025-07-05 01:24:44 +0530

    file removed

--- library.py
+++
@@ -1,9 +0,0 @@
-#no of books
-#no of users
-#no of staffs
-
-class Library():
-    def __init__(self, books, users, staffs):
-        self.total_books = books
-        self.total_users = users
-        self.total_staffs = staffs
